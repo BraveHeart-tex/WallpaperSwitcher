3dce6cc1bbcce1f75de8399b88261c72bf910774
Bora Karaca <bora.karaca@ogr.iu.edu.tr>
2026-04-11 10:14:14 +0300

    Initial Commit

---
+++ WallpaperSwitcher/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WallpaperSwitcher
+//
+//  Created by Bora on 11.04.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WallpaperSwitcher/WallpaperSwitcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WallpaperSwitcherApp.swift
+//  WallpaperSwitcher
+//
+//  Created by Bora on 11.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct WallpaperSwitcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
